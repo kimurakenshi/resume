183b4148db3d00771d789d7035686a538be8b57b
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-01-25 12:39:09 -0800

    update vercel url

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="h-1.5 contents text-center">
       <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        href="https://vercel.com"
         target="_blank"
         rel="noopener noreferrer"
         className="text-white mb-2"
