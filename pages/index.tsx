6a8740a80fa33f30894675ee7bba6522cb3551e1
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-01-25 11:48:59 -0800

    Under construction version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,23 @@
-import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
-  const coso = 'coso';
-  return <div className="bg-gray-500">Something</div>;
+  return (
+    <main className="bg-black h-screen flex flex-col justify-center self-center">
+      <h1 className="text-center font-sans font-bold text-white text-6xl mb-8">
+        Hi! I&apos;m Sebastian Giraudo.
+      </h1>
+
+      <p className="text-center font-sans text-gray-300 text-3xl mb-8">
+        This&apos;ll be my personal Website soon!
+      </p>
+
+      <Image
+        className="animate-pulse"
+        alt="Site under construction"
+        src="/under-construction.svg"
+        width={300}
+        height={250}
+      />
+    </main>
+  );
 }
