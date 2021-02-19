4553e65d48649d9b048469460b18d42dbfbb6a0d
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-02-19 15:32:35 -0800

    fix arrow icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,34 +96,32 @@ const Home = () => {
 
         {state.hasIntroPageLoaded && (
           <>
-            <p className="z-10">Hello. I&apos;m Sebastián.</p>
-            <p className="z-10">This is my personal Website.</p>
+            <p>Hello. I&apos;m Sebastián.</p>
+            <p>This is my personal Website.</p>
           </>
         )}
 
         {state.hasIntroPageLoaded && (
-          <div className="flex justify-center h-8 mt-4">
-            <svg
-              className={classNames(
-                'fill-current animate__animated animate__shakeY animate__slower animate__infinite',
-                {
-                  'text-white': state.theme === Theme.DARK,
-                  'text-black': state.theme !== Theme.DARK,
-                }
-              )}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M19 14l-7 7m0 0l-7-7m7 7V3"
-              />
-            </svg>
-          </div>
+          <svg
+            className={classNames(
+              'fill-current animate__animated animate__shakeY animate__slower animate__infinite h-8 mt-4',
+              {
+                'text-white': state.theme === Theme.DARK,
+                'text-black': state.theme !== Theme.DARK,
+              }
+            )}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 14l-7 7m0 0l-7-7m7 7V3"
+            />
+          </svg>
         )}
       </div>
 
