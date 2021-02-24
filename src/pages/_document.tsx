430589089a9943b21158adfd9dc3c7f1343dee72
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-02-24 11:43:19 -0800

    Add animate css as a dependency

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,16 +3,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;300;400;500;700&display=swap"
             rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
           />
         </Head>
         <body className="background">
