029b46ece8cbb82e1f021e7768c96354606212a6
Sebastian Giraudo <sebastian.giraudo@globant.com>
2021-03-01 14:12:45 -0800

    fancy home page initial approach

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,12 @@ const url = (name, wrap = false) =>
 const shortPause = 400;
 const longPause = 1000;
 
+enum PAGE {
+  FIRST = 0,
+  SECOND = 1,
+  THIRD = 2,
+}
+
 const backgroundColorsForTheme = {
   [Theme.DARK]: [
     {
@@ -48,20 +54,9 @@ const Home = () => {
 
   return (
     <Page description="Home" title="Home" hasSpacing={false}>
-      <Parallax ref={parallaxRef} pages={3} scrolling={false}>
+      <Parallax ref={parallaxRef} pages={3} scrolling={true}>
         <ParallaxLayer
-          offset={1}
-          speed={1}
-          style={{ backgroundColor: '#805E73' }}
-        />
-        <ParallaxLayer
-          offset={2}
-          speed={1}
-          style={{ backgroundColor: '#87BCDE' }}
-        />
-
-        <ParallaxLayer
-          offset={0}
+          offset={PAGE.FIRST}
           speed={0}
           factor={3}
           style={{
@@ -71,100 +66,7 @@ const Home = () => {
         />
 
         <ParallaxLayer
-          offset={1.3}
-          speed={-0.3}
-          style={{ pointerEvents: 'none' }}
-        >
-          <img
-            src={url('satellite4')}
-            style={{ width: '15%', marginLeft: '70%' }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '20%', marginLeft: '55%' }}
-          />
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '10%', marginLeft: '15%' }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '20%', marginLeft: '70%' }}
-          />
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '20%', marginLeft: '40%' }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '10%', marginLeft: '10%' }}
-          />
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '20%', marginLeft: '75%' }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '20%', marginLeft: '60%' }}
-          />
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '25%', marginLeft: '30%' }}
-          />
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '10%', marginLeft: '80%' }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '20%', marginLeft: '5%' }}
-          />
-          <img
-            src={url('cloud')}
-            style={{ display: 'block', width: '15%', marginLeft: '75%' }}
-          />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2.5}
-          speed={-0.4}
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            pointerEvents: 'none',
-          }}
-        >
-          <img src={url('earth')} style={{ width: '60%' }} />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2}
-          speed={-0.3}
-          style={{
-            backgroundSize: '80%',
-            backgroundPosition: 'center',
-            backgroundImage: url('clients', true),
-          }}
-        />
-
-        <ParallaxLayer
-          offset={0}
+          offset={PAGE.FIRST}
           speed={0.1}
           onClick={() => parallaxRef.current.scrollTo(1)}
           style={{
@@ -239,20 +141,162 @@ const Home = () => {
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={1}
+          offset={PAGE.SECOND}
+          speed={1}
+          style={{ backgroundColor: '#805e73' }}
+        />
+        <ParallaxLayer
+          offset={1.3}
+          speed={-0.3}
+          style={{ pointerEvents: 'none' }}
+        >
+          <img
+            src={url('satellite4')}
+            style={{ width: '15%', marginLeft: '70%' }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.SECOND}
+          speed={0.8}
+          style={{ opacity: 0.1 }}
+        >
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '20%', marginLeft: '55%' }}
+          />
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '10%', marginLeft: '15%' }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.SECOND}
+          speed={0.2}
+          style={{ opacity: 0.2 }}
+        >
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '10%', marginLeft: '10%' }}
+          />
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '20%', marginLeft: '75%' }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.THIRD - 0.25}
+          speed={0.5}
+          style={{ opacity: 0.1 }}
+        >
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '20%', marginLeft: '70%' }}
+          />
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '20%', marginLeft: '40%' }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.SECOND}
           speed={0.1}
           onClick={() => parallaxRef.current.scrollTo(2)}
           style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            flexDirection: 'column',
           }}
         >
-          <img src={url('bash')} style={{ width: '40%' }} />
+          <Panel customClasses="mb-10 md:mb-20">
+            I&apos;m a System Engineer from Argentina. I moved to US on 2014
+            with my dog Charlie to continue my career path in the tech world.
+          </Panel>
+
+          <Panel customClasses="mb-10 md:mb-20">
+            I enjoy building new things, I&apos;m passionated about UI/UX and
+            helping my team to succeed.
+          </Panel>
+
+          <Panel customClasses="mb-10 md:mb-20">
+            I love music, reading, video games and dogs.
+          </Panel>
+
+          <Panel customClasses="mb-10 md:mb-20">
+            I&apos;m always looking for new challenges that can keep me
+            motivated and help me to learn new things.
+          </Panel>
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2}
+          offset={PAGE.THIRD}
+          speed={1}
+          style={{ backgroundColor: '#87BCDE' }}
+        />
+
+        <ParallaxLayer
+          offset={PAGE.THIRD - 0.4}
+          speed={-0.1}
+          style={{ opacity: 0.4 }}
+        >
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '20%', marginLeft: '60%' }}
+          />
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '25%', marginLeft: '30%' }}
+          />
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '10%', marginLeft: '80%' }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.THIRD + 0.6}
+          speed={0.4}
+          style={{ opacity: 0.6 }}
+        >
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '20%', marginLeft: '5%' }}
+          />
+          <img
+            src={url('cloud')}
+            style={{ display: 'block', width: '15%', marginLeft: '75%' }}
+          />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.THIRD + 0.5}
+          speed={-0.4}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            pointerEvents: 'none',
+          }}
+        >
+          <img src={url('earth')} style={{ width: '60%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={PAGE.THIRD}
+          speed={-0.3}
+          style={{
+            backgroundSize: '80%',
+            backgroundPosition: 'center',
+            backgroundImage: url('clients', true),
+          }}
+        />
+
+        <ParallaxLayer
+          offset={PAGE.THIRD}
           speed={-0}
           style={{
             display: 'flex',
